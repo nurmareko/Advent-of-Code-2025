02233817944d4ad65b03412f4a74a4c02d804c81
nurmareko <nurmareko@yandex.ru>
2026-07-18 00:09:03 +0700

    Implementation of crack_password and turn_dial functions

--- day_01/main.cc
+++ day_01/main.cc
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,27 +7,27 @@ using namespace std;
 
 class Rotation {
     private:
-        char direction;
-        int distance;
-
         // TODO
-        char get_direction(string str)
+        char parse_direction(string str)
         {
             return 'L';
         }
 
         // TODO
-        int get_distance(string str)
+        int parse_distance(string str)
         {
             return 1;
         }
 
     public:
+        char direction;
+        int distance;
+
         // TODO
         Rotation(string str)
         {
-            direction = get_direction(str);
-            distance = get_distance(str);
+            direction = parse_direction(str);
+            distance = parse_distance(str);
         }
 };
 
@@ -43,17 +44,86 @@ vector<Rotation> collect_rotation()
     return result;
 }
 
-// TODO
-int crack_password(vector<Rotation>)
+int turn_dial(int dial, Rotation rotation)
 {
-    return 0;
+    char direction = rotation.direction;
+    int distance = rotation.distance;
+
+    int result = dial;
+
+    if (distance == 0) {
+        return dial;
+    }
+
+    if (direction == 'L') {
+        while (distance > 0) {
+            if (dial == 0) {
+                dial = 99;
+            } else {
+                --dial;
+            }
+            --distance;
+        }
+    } else {
+        while (distance > 0) {
+            if (dial == 99) {
+                dial = 0;
+            } else {
+                ++dial;
+            }
+            --distance;
+        }
+    }
+
+    return result;
+}
+
+int crack_password(vector<Rotation> puzzle)
+{
+    int dial = 50;
+    int result = 0;
+
+    for (Rotation rotation : puzzle) {
+        dial = turn_dial(dial, rotation);
+
+        if (dial == 0) {
+            ++result;
+        }
+    }
+
+    return result;
+}
+
+void test_case()
+{
+    vector<Rotation> puzzle1;
+    vector<Rotation> puzzle2 = {Rotation("L35")};
+    vector<Rotation> puzzle3 = {Rotation("L50")};
+    vector<Rotation> puzzle4 = {Rotation("R50")};
+    vector<Rotation> puzzle5 = {Rotation("L40"), Rotation("L10")};
+    assert(crack_password(puzzle1) == 0);
+    assert(crack_password(puzzle2) == 0);
+    assert(crack_password(puzzle3) == 1);
+    assert(crack_password(puzzle4) == 1);
+    assert(crack_password(puzzle5) == 1);
+
+    assert(turn_dial(50, Rotation("L0")) == 50);
+    assert(turn_dial(50, Rotation("L15")) == 35);
+    assert(turn_dial(50, Rotation("R10")) == 60);
+    assert(turn_dial(50, Rotation("L50")) == 0);
+    assert(turn_dial(50, Rotation("L51")) == 99);
+    assert(turn_dial(50, Rotation("R49")) == 99);
+    assert(turn_dial(50, Rotation("L50")) == 0);
+
+    cout << "tests passed!" << endl;
 }
 
 int main()
 {
-    vector<Rotation> puzzle = collect_rotation();
+    test_case();
 
-    cout << "password: " << crack_password(puzzle) << endl;
+    // vector<Rotation> puzzle = collect_rotation();
+    // cout << "password: " << crack_password(puzzle) << endl;
 
     return 0;
 }
