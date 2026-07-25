e67ce381645cf20209bcac615af687236f47b48d
nurmareko <nurmareko@yandex.ru>
2026-07-25 19:57:34 +0700

    day 2 stubs

--- day_02/main.cc
+++ day_02/main.cc
@@ -1,10 +1,37 @@
+#include <algorithm>
 #include <iostream>
+#include <numeric>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
+vector<pair<int, int>> parse_input();
+vector<int> get_invalids(vector<pair<int, int>> ranges);
+
 int main()
 {
-    cout << "Hello, day 2" << endl;
+    vector<pair<int, int>> ranges = parse_input();
+
+    vector<int> invalids = get_invalids(ranges);
+
+    int invalid_sum = accumulate(
+        invalids.begin(),
+        invalids.end(),
+        0
+    );
+
+    cout << "result: " << invalid_sum << endl;
 
     return 0;
+}
+
+// TODO
+vector<pair<int, int>> parse_input() {
+    return {};
+}
+
+// TODO
+vector<int> get_invalids(vector<pair<int, int>> ranges) {
+    return {};
 }
